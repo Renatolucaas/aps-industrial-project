4089f0f81dffa0d8fb672d73754d3a966515c493
RenatoLucas <renatolucas918@gmail.com>
2026-09-06 15:45:51 -0300

    criando classe machine connectionalAPS.Domain\Entities\MachineConnection.cs

---
+++ IndustrialAPS.Domain/Entities/MachineConnection.cs
@@ -0,0 +1,13 @@
+﻿namespace IndustrialAPS.Domain.Entities
+{
+    public class MachineConnection
+    {
+        public int Id { get; set; }
+        public int FromMachineId { get; set; }
+        public int ToMachineId { get; set; }
+        public decimal Weight { get; set; } // Distância, tempo ou custo
+
+        public Machine FromMachine { get; set; } = null!;
+        public Machine ToMachine { get; set; } = null!;
+    }
+}
