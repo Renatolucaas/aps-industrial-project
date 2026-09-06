1650a68e141d054b9148bd02d80458318e228eb0
RenatoLucas <renatolucas918@gmail.com>
2026-09-06 15:31:35 -0300

    criando classe material

--- IndustrialAPS.Domain/obj/Debug/net8.0/IndustrialAPS.Domain.AssemblyInfo.cs
+++ IndustrialAPS.Domain/obj/Debug/net8.0/IndustrialAPS.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IndustrialAPS.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6d3587cf657b68e1466bec91527b3a5486f896f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f0636b4cf67cccf1a0af015e3b01792997a4e2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("IndustrialAPS.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IndustrialAPS.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
