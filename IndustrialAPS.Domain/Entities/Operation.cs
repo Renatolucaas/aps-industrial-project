8284dd9d9f24eebc1a546f6c6ebbdde6196dcc54
RenatoLucas <renatolucas918@gmail.com>
2026-09-06 16:01:55 -0300

    criando classe operation

---
+++ IndustrialAPS.Domain/Entities/Operation.cs
@@ -0,0 +1,14 @@
+﻿namespace IndustrialAPS.Domain.Entities
+{
+    public class Operation
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Duration { get; set; } // em minutos
+
+        public Product Product { get; set; } = null!;
+        public ICollection<MachineOperation> Machines { get; set; } = new List<MachineOperation>();
+        public ICollection<Schedule> Schedules { get; set; } = new List<Schedule>();
+    }
+}
