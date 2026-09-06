5ecd2df7693ce7a82cb97ea0a3dae00856fab1e8
RenatoLucas <renatolucas918@gmail.com>
2026-09-06 15:50:24 -0300

    criando classe machine operation

---
+++ IndustrialAPS.Domain/Entities/MachineOperation.cs
@@ -0,0 +1,11 @@
+﻿namespace IndustrialAPS.Domain.Entities
+{
+    public class MachineOperation
+    {
+        public int MachineId { get; set; }
+        public int OperationId { get; set; }
+
+        public Machine Machine { get; set; } = null!;
+        public Operation Operation { get; set; } = null!;
+    }
+}
