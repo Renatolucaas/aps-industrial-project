2aec4af564110835cab7b0fe00ceb2004a319093
RenatoLucas <renatolucas918@gmail.com>
2026-09-06 16:07:15 -0300

    criando classe production ordem

---
+++ IndustrialAPS.Domain/Entities/ProductionOrder.cs
@@ -0,0 +1,23 @@
+﻿namespace IndustrialAPS.Domain.Entities
+{
+    public class ProductionOrder
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public decimal Quantity { get; set; }
+        public DateTime DueDate { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public Product Product { get; set; } = null!;
+        public ICollection<Schedule> Schedules { get; set; } = new List<Schedule>();
+    }
+
+    public enum OrderStatus
+    {
+        Created,
+        Planned,
+        InProduction,
+        Completed,
+        Delayed
+    }
+}
