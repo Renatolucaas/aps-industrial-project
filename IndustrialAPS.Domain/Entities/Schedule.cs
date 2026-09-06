21a96f9ec9b126ecadc427322c543978f10a4c30
RenatoLucas <renatolucas918@gmail.com>
2026-09-06 16:08:02 -0300

    criando classe schedule

---
+++ IndustrialAPS.Domain/Entities/Schedule.cs
@@ -0,0 +1,16 @@
+﻿namespace IndustrialAPS.Domain.Entities
+{
+    public class Schedule
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public int OperationId { get; set; }
+        public int MachineId { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+
+        public ProductionOrder Order { get; set; } = null!;
+        public Operation Operation { get; set; } = null!;
+        public Machine Machine { get; set; } = null!;
+    }
+}
