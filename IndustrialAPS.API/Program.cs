e137c8fc2484fae3c29512a5f7d7f4f58e69ab7a
RenatoLucas <renatolucas918@gmail.com>
2026-09-07 10:55:31 -0300

    configurando canal de comunicação entre API e banco de dados

--- IndustrialAPS.API/Program.cs
+++ IndustrialAPS.API/Program.cs
@@ -1,11 +1,17 @@
+using IndustrialAPS.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configura o DbContext com a string de conexão
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
