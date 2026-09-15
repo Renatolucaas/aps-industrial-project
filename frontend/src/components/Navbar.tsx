ff27bf471863bf374b2e1faa78d30525da20e8c9
RenatoLucas <renatolucas918@gmail.com>
2026-09-15 09:35:17 -0300

    feat: components Navbar.tsx

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">Industrial APS</Link>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item"><Link className="nav-link" to="/">Dashboard</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/products">Produtos</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/machines">Máquinas</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/materials">Materiais</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/bom">BOM</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/planning">Planejamento</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/tree">Árvore</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/graph">Grafo</Link></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
